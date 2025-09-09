6ea01421aafe3d008e73c035617a4be4d4f34e8d
jacpa42 <jacobenthoven@gmail.com>
2025-09-09 21:53:08 +0200

    fixed build

--- build.zig
+++ build.zig
@@ -3,11 +3,20 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
     const mod = b.addModule("qoi", .{
         .root_source_file = b.path("src/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    const lib = b.addLibrary(.{
+        .name = "qoi",
+        .linkage = .static,
+        .root_module = mod,
+    });
+
+    b.installArtifact(lib);
 
     const mod_tests = b.addTest(.{ .root_module = mod });
     const run_mod_tests = b.addRunArtifact(mod_tests);
