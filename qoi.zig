104295495280ebf3509382e16398c6d8a247d279
jacpa42 <jacobenthoven@gmail.com>
2025-09-09 11:09:52 +0200

    Inital commit (history remove)

---
+++ qoi.zig
@@ -0,0 +1,8 @@
+const qoi = @import("src/lib.zig");
+
+pub const QOI = qoi.QOI;
+pub const decode = qoi.decode;
+pub const encode = qoi.encode;
+pub const Channels = qoi.Channels;
+pub const ColorSpace = qoi.ColorSpace;
+pub const rgba = qoi.rgba;
