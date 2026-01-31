1a84fce189dba3da89da3c1d7a007104390222a5
jacpa42 <jacobenthoven@gmail.com>
2026-01-31 08:02:29 +0100

    modified build zig slightly

--- build.zig
+++ build.zig
@@ -4,19 +4,13 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const libqoi = b.addLibrary(.{
-        .name = "qoi",
-        .linkage = .static,
-        .root_module = b.addModule("qoi", .{
-            .root_source_file = b.path("QOI.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+    const modqoi = b.addModule("qoi", .{
+        .root_source_file = b.path("qoi.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
-    b.installArtifact(libqoi);
-
-    const mod_tests = b.addTest(.{ .root_module = libqoi.root_module });
+    const mod_tests = b.addTest(.{ .root_module = modqoi });
     const run_mod_tests = b.addRunArtifact(mod_tests);
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
